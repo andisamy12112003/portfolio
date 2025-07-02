b2ca4df63133a76d1617538c58db5a6e1f2207bb
ANDISAMY <ANDISAMY>
2025-07-02 07:40:12 +0530

    update hover effect project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
                 {/*project one */}
                 <a href="https://jobby-eta-two.vercel.app/" target="__black" className="max-sm:mt-10 m-7">
                         <img src={project1} className="xl:w-140 lg:w-190 lg:h-120 lg:hover:shadow-2xl
-                        max-lg:w-120 max-lg:h-120 max-lg:hover:size-115 max-lg:shadow-2xl max-md:w-90 max-md:h-90 max-md:hover:size-87 max-md:shadow-2xl rounded-2xl max-sm:h-50" />
+                        max-lg:w-120 max-lg:h-120 max-lg:hover:w-92 max-lg:hover:mt-2 max-lg:shadow-2xl max-md:w-90 max-md:h-90 max-md:shadow-2xl rounded-2xl max-sm:h-50" />
                         <div className="mt-9 ">
                         <h1 className="font-mono text-[16px] max-sm:-mt-6 max-lg:text-[22px] lg:text-[24px]"><span className="font-bold">Jobby</span> - Job Searching website</h1> 
                         <div  className="w-full text-sm max-lg:mt-2 mb-2 lg:text-[19px] lg:mt-2">   
@@ -24,7 +24,7 @@ const Projects = () => {
                 {/*project two*/}
                 <a className="max-sm:mt-10 m-7" href="https://next-trendz.vercel.app/" target="__black">
                         <img src={project2} className="xl:w-140 lg:w-190  lg:h-120 lg:hover:shadow-2xl
-                        max-lg:w-120 max-lg:h-120 max-lg:shadow-2xl max-md:w-90 max-md:h-90 max-md:shadow-2xl rounded-2xl max-sm:h-50"/>
+                        max-lg:w-120 max-lg:h-120 max-lg:shadow-2xl max-md:w-90 max-md:h-90 max-md:shadow-2xl rounded-2xl max-sm:h-50 max-lg:hover:w-92 max-lg:hover:mt-2"/>
                         <div className="mt-9 ">
                         <h1 className="font-mono text-[16px] max-sm:-mt-6 max-lg:text-[22px] lg:text-[24px]"><span className="font-bold">Next Trendz</span> - Video watching website</h1> 
                         <div  className="w-full text-sm max-lg:mt-2 mb-2 lg:text-[19px] lg:mt-2">   
